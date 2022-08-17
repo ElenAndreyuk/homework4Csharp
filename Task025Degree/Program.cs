88d44bf70a85fe6371cccf47e3c9ca7b62e3de6a
Elena <femtochka@yahoo.com>
2022-08-17 15:58:44 +0300

    task 025

---
+++ Task025Degree/Program.cs
@@ -0,0 +1,23 @@
+﻿// Возводим число А в натуральную степень числа B
+Console.WriteLine("Введите число А");
+try
+{
+    int A = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите число B");
+    int B = int.Parse(Console.ReadLine());
+
+    int degree = 1;
+
+    for(int i = 1; i <= B; i++)
+    {
+        degree = degree * A;
+        
+    }
+    Console.WriteLine(degree);
+}
+
+catch
+{
+    System.Console.WriteLine("это не число");
+}
+
