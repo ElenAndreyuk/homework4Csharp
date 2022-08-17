10b581a8d5af7cc2d8c4ba7cad93a0c5d6733e5e
Elena <femtochka@yahoo.com>
2022-08-17 16:31:45 +0300

    task 027

---
+++ Task027SumOfDigit/Program.cs
@@ -0,0 +1,18 @@
+﻿// Сумма цифр в числе
+Console.WriteLine("Введите число");
+try
+{
+    int number = int.Parse(Console.ReadLine());
+    int sum = 0;
+    while(number != 0)
+    {
+        sum = sum + (number % 10);
+        number = number / 10;
+        Console.WriteLine(number);  
+        Console.WriteLine(sum);
+    }
+}
+catch
+{
+    Console.WriteLine("Это не число");
+}
